c1e01bc1d5dd729072ba31018a660a0658b4a5e6
Cmauricio Parra <cmauricio@yahoo.com.mx>
2024-08-20 23:41:08 -0500

    Add: src/config/constants.ts

---
+++ src/config/constants.ts
@@ -0,0 +1,5 @@
+export const MAIN_STORAGE = 'storage/';
+export const PRODUCT_DIR = 'products/';
+export const PRODUCT_IMG_URL = `/${PRODUCT_DIR}img/`;
+export const PRODUCT_IMG_PATH = `static/${PRODUCT_DIR}img/`;
+export const PRODUCT_FILE_PATH = `${MAIN_STORAGE}${PRODUCT_DIR}`;
