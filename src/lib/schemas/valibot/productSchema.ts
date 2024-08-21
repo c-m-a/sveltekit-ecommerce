7548bb67f4f1dfda12b1b8ce35ee0079c826c070
Cmauricio Parra <cmauricio@yahoo.com.mx>
2024-08-20 22:06:02 -0500

    Add: src/lib/schemas/valibot/productSchema.ts

---
+++ src/lib/schemas/valibot/productSchema.ts
@@ -0,0 +1,28 @@
+import {
+	object,
+	minLength,
+	pipe,
+	string,
+	number,
+	toMinValue,
+	file,
+	maxSize,
+	mimeType
+} from 'valibot';
+
+const FILE_MAX_SIZE = 1024 * 1024 * 10; // 10MB
+
+export const addProductSchema = object({
+	name: pipe(string(), minLength(3, 'message')),
+	price: pipe(number(), toMinValue(0)),
+	description: pipe(string(), minLength(5)),
+	file: pipe(
+		file('Please select a file!'),
+		maxSize(FILE_MAX_SIZE, 'Please select a file smaller than 10 MB!')
+	),
+	image: pipe(
+		file('Please select a file'),
+		mimeType(['image/jpeg', 'image/png'], 'Please select a JPEG or PNG file.'),
+		maxSize(1024 * 1024 * 10, 'Please select a file smaller than 10 MB.')
+	)
+});
