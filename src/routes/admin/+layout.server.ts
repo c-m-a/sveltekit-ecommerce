369453d4439891ffd905abdcef20be024b80f3db
Cmauricio Parra <cmauricio@yahoo.com.mx>
2024-08-26 09:58:31 -0500

    Refactor: improve redirect mechanism when admin is not authenticated

--- src/routes/admin/+layout.server.ts
+++ src/routes/admin/+layout.server.ts
@@ -1,7 +1,10 @@
+import { handleLoginRedirect } from '@lib/utils';
 import { redirect } from '@sveltejs/kit';
 
-export const load = async ({ locals }) => {
-	if (!locals.user) {
-		redirect(302, '/login');
+export const load = async (e) => {
+	if (!e.locals.user) {
+		// redirect the path user requested
+		const fromUrl = e.url.pathname + e.url.search;
+		throw redirect(302, handleLoginRedirect(e));
 	}
 };
