bd251b9c82c84219f4aab6051a10bd062e2240d3
Cmauricio Parra <cmauricio@yahoo.com.mx>
2024-08-25 21:47:53 -0500

    Add: prisma/migrations/20240825205328_add_expire_at_to_session/

---
+++ prisma/migrations/20240825205328_add_expire_at_to_session/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - Added the required column `expiredAt` to the `Session` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Session" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expiredAt" DATETIME NOT NULL,
+    CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Session" ("createdAt", "id", "userId") SELECT "createdAt", "id", "userId" FROM "Session";
+DROP TABLE "Session";
+ALTER TABLE "new_Session" RENAME TO "Session";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
